c4b3cd1854501beb246cae2621afa64686ffaf9f
Diego Calderon <diegocaldeleon1@gmail.com>
2019-11-06 13:03:04 -0600

    Modificacion del ejemplo de red para usar PrintWriter en lugar de DataOutputStream

--- network/Client.java
+++ network/Client.java
@@ -1,5 +1,5 @@
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
+import java.io.PrintWriter;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
@@ -10,10 +10,11 @@ public class Client {
             InputStreamReader isr = new InputStreamReader(socketServer.getInputStream());
             BufferedReader in = new BufferedReader(isr);
 
-            DataOutputStream out = new DataOutputStream(socketServer.getOutputStream());
+            // es importante el segundo argumento (true) para que tenga autoflush al hacer print
+            PrintWriter out = new PrintWriter(socketServer.getOutputStream(), true);
 
             System.out.println("Servidor: " + in.readLine());
-            out.writeUTF("Gracias, por aceptar mi solicitud Sr. Servidor\n");
+            out.println("Gracias, por aceptar mi solicitud Sr. Servidor");
 
             in.close();
             out.close();
--- network/Server.java
+++ network/Server.java
@@ -1,6 +1,6 @@
 import java.io.BufferedReader;
 import java.io.DataOutput;
-import java.io.DataOutputStream;
+import java.io.PrintWriter;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -16,10 +16,11 @@ public class Server {
             
             InputStreamReader isr = new InputStreamReader(socketClient.getInputStream());
             BufferedReader in = new BufferedReader(isr);
+            
+            // es importante el segundo argumento (true) para que tenga autoflush al hacer print
+            PrintWriter out = new PrintWriter(socketClient.getOutputStream(), true);
     
-            DataOutputStream out = new DataOutputStream(socketClient.getOutputStream());
-    
-            out.writeUTF("Bienvenido al servidor de cc2 :)\n");
+            out.println("Bienvenido al servidor de cc2 :)");
             System.out.println("Cliente: " + in.readLine());
 
             in.close();
